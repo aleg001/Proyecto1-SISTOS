de0c98d5e21ae24d1230c8d7b15c6f4fffefcd86
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2023-04-30 21:08:05 -0600

    Update server.c

--- server.c
+++ server.c
@@ -8,10 +8,63 @@
 #include <netinet/in.h> //Direcciones de red
 #include <pthread.h> //Hilos
 #include <arpa/inet.h> // Manipulacion direcciones IP
-#include "chat.pb-c.h"
+#include "chat.pb-c.h" // Protobuf
 
-void handle_newuser(){
+#define MAX_CLIENTS 50
 
+// Estructura basada en el .proto
+typedef struct {
+    char username[75];
+    char user_ip[20];
+    int status;
+    int sockfd;
+} Client;
+
+Client clients[MAX_CLIENTS]; // Lista de clientes
+int cantidad_clientes = 0;
+
+void* handle_newclient(void *arg){
+
+    // Se obtiene el cliente y su informacion
+    int client_socket = *(int*)arg;
+    uint8_t buffer[1024];
+    ssize_t bytes_received = recv(client_socket, buffer, 1024, 0);
+
+    // Se verifica si existe informacion
+    if (bytes_received < 0){
+        perror("[SERVER-ERROR]: Recepcion de cliente fallida\n");
+        exit(EXIT_FAILURE);
+    } else {
+        // Se crea un nuevo usuario en el server para manejar mejor la informacion
+        Chat__User* new_client = chat__new_user__unpack(NULL, bytes_received, buffer); 
+
+        if(new_client == NULL){
+            perror("[SERVER-ERROR]: Registro de usuario fallido\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // Se imprime informacion y se libera memoria
+        printf("Bienvenido: %s de ip: %s\n", new_client->user_name, new_client->user_ip);
+
+        // Respuesta del servidor
+        
+
+        // Se agrega el nuevo cliente a la lista
+        Client client;
+        strcpy(client.username, new_client->user_name);
+        strcpy(client.user_ip, new_client->user_ip);
+        client.status = 1;
+        client.sockfd = client_socket;
+
+        clients[cantidad_clientes] = client;
+        cantidad_clientes++;
+
+        printf("Lista de clientes conectados:\n");
+        for (int i = 0; i < cantidad_clientes; i++) {
+            printf("- %s (%s)\n", clients[i].username, clients[i].user_ip);
+        }
+        chat__new_user__free_unpacked(new_client, NULL);
+    }
 }
 
 
@@ -19,7 +72,7 @@ int main(int argc, char **argv) {
 
     if(argc > 1){
         // descriptor del socket del servidor y cliente, puerto y la cantidad de bytes leidos
-        int server_fd, cliente_fd, puerto, valread;
+        int server_fd, client_fd, puerto, valread;
 
         // se obtiene el puerto de los argumentos
         puerto = atoi(argv[1]);
@@ -66,7 +119,7 @@ int main(int argc, char **argv) {
         // listen espera las conexiones
         // primer argumento es el socket del servidor
         // segundo argumento la capacidad de cola
-        if (listen(server_fd, 10) != 0) {
+        if (listen(server_fd, MAX_CLIENTS) != 0) {
             perror("[SERVER-ERROR]: Socket listen fallido");
             exit(EXIT_FAILURE);    
         } else {
@@ -74,31 +127,25 @@ int main(int argc, char **argv) {
         }
 
         while (1) {
-
-            cliente_fd = accept(server_fd, (struct sockaddr *)&client, (socklen_t*)&addrlenclient);
-            if (cliente_fd < 0) {
+            client_fd = accept(server_fd, (struct sockaddr *)&client, (socklen_t*)&addrlenclient);
+            if (client_fd < 0) {
                 perror("[SERVER-ERROR]: Conexion no aceptada\n");
                 exit(EXIT_FAILURE);
-            } else {
-                char* client_ip = inet_ntoa(client.sin_addr);
-                while (1) {
-                    valread = read(cliente_fd, buffer_rx, sizeof(buffer_rx));
-
-                    if(valread == -1){
-                        fprintf(stderr, "[SERVER-ERROR]: FD del cliente no pudo leerse\n");
-                    } else if(valread == 0) {
-                        printf("[SERVER]: Socket del cliente cerrado\n\n");
-                        close(cliente_fd);
-                        break;
-                    } else {
-                        write(cliente_fd, buffer_tx, strlen(buffer_tx));
-                        printf("[SERVER]: %s conectado desde la ip: %s\n", buffer_rx, inet_ntoa(client.sin_addr));
-                    }
-                }
             }
+
+            // printf("[SERVER]: %s conectado desde la ip: %s y puerto: %d\n", buffer_rx, inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+       
+            pthread_t thread;
+            int pthread_result = pthread_create(&thread, NULL, handle_newclient, (void *)&client_fd);
+            if(pthread_result < 0){
+                perror("[SERVER-ERROR]: Creacion de hilo para cliente fallida\n");
+                exit(EXIT_FAILURE);
+            }
+        
         }
 
         return 0;
+
     } else {
         printf("Puerto no especificado. Comando: <Nombre Programa> <Puerto>\n");
     }
